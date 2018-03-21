bbfcc56a11533936b037df1d08d4eb9069eaae60
Michael Pan <scentsome@gmail.com>
2018-03-21 09:06:28 +0800

    Create Demo project day 4

---
+++ Demo4/Demo4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo4
+//
+//  Created by Michael on 21/03/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
