dab690ed9000d8075b2ac019506385876a459942
Michael Pan <scentsome@gmail.com>
2018-03-27 10:30:19 +0800

    Demo closure for delegate

---
+++ Demo6/Demo6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo6
+//
+//  Created by Michael on 27/03/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
