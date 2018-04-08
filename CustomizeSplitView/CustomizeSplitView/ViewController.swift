90753673b6cf47f7c8c68d73a7a6d246fde280ce
Michael Pan <scentsome@gmail.com>
2018-04-08 12:42:58 +0800

    Add custom split View

---
+++ CustomizeSplitView/CustomizeSplitView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomizeSplitView
+//
+//  Created by Michael on 08/04/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
