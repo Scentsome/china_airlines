be594de4eff9abcc157d89dae567cdfaa81f3f78
Michael Pan <scentsome@gmail.com>
2018-04-08 13:02:04 +0800

    Add iboutlet and setting storyborad

---
+++ CustomizeSplitView/CustomizeSplitView/LeftViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LeftViewController.swift
+//  CustomizeSplitView
+//
+//  Created by Michael on 08/04/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class LeftViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CustomizeSplitView/CustomizeSplitView/RightViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RightViewController.swift
+//  CustomizeSplitView
+//
+//  Created by Michael on 08/04/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class RightViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
