115237d4ee73af9de38145686d0678a5e6541a5e
Michael Pan <scentsome@gmail.com>
2018-04-12 09:44:28 +0800

    Add main storyboard

--- StackViewDemo/StackViewDemo/ViewController.swift
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var stackCenterX: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
+//        numLabel.layer.cornerRadius = 10
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        numLabel.layer.cornerRadius = numLabel.frame.size.width / 2 
+        numLabel.layer.borderWidth = 5
+        numLabel.layer.borderColor = UIColor.blue.cgColor
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        if size.width > size.height {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.stackCenterX.constant = 100
+            })
+            
+        }else {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.stackCenterX.constant = 0
+                })
+        }
+    }
 
 }
 
