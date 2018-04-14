3b91aa4c91a22ed4142e64e6971893c39314fb7a
Michael Pan <scentsome@gmail.com>
2018-04-14 09:25:51 +0800

    Add login demo

---
+++ LoginDemo/LoginDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginDemo
+//
+//  Created by Michael on 14/04/2018.
+//  Copyright © 2018 Zencher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
