1854af8ad1e19b6c57f50e19e970fdb237c3c4c4
Michael Pan <scentsome@gmail.com>
2018-04-14 10:28:24 +0800

    Import Kanna

--- LoginDemo/LoginDemo/ViewController.swift
+++ LoginDemo/LoginDemo/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import Kanna
 
 class ViewController: UIViewController {
     @IBOutlet weak var empNoTextfield: UITextField!
@@ -20,6 +22,34 @@ class ViewController: UIViewController {
         
         if empNoTextfield.text! != "" && pwdTextfield.text! != "" && syspwdTextfield.text! != "" {
             
+            let empNo:String = empNoTextfield.text!
+            let pwd:String = pwdTextfield.text!
+            let syspwd:String = syspwdTextfield.text!
+            let parameters = ["empno":empNo,"pwd":pwd,"syspwd":syspwd]
+            
+            let soap = Soap(url: "https://tpecr01.china-airlines.com/test/jsonparser/jsonoutput_1/jsonoutput_1/Service1.asmx", parameter: parameters, methodName: "LoginCheckXML", namespace: "http://tempuri.org/")
+            
+            let request = soap.post12()
+
+//            print(String(data: request.httpBody!, encoding: String.Encoding.utf8))
+            Alamofire.request(request).response { (response) in
+                if let data = response.data, let utf8Text = String(data: data,encoding: .utf8) {
+                    print("Data: \(utf8Text)")
+                    
+                    do{
+                        let doc = try Kanna.XML(xml: data, encoding: .utf8)
+                        let resultNode = doc.at_xpath("//ns:result",namespaces: ["ns":"http://tempuri.org/"])
+                        if resultNode != nil {
+                            let result:String = (resultNode?.text)!
+                            print(result)
+                        }
+                    } catch {
+                        print("Parser Error")
+                    }
+
+                }
+            }
+
         }else {
             
         }
